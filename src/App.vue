fe18adeb191dab2ab9afd673920564e33d3c6db5
serhiichaiuk <chayck7@gmail.com>
2022-07-25 18:55:36 +0300

    imported component with props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <div id="main-app" class="container">
-    <h4>{{ title }}</h4>
-    <font-awesome-icon icon="plus" class="mr-2"/>
-    Add Appointment
-    <div v-for="(item, i) in appointments" v-bind:key="i">
-      <h4>{{ item.petName }}</h4>
-      <p>{{ item.aptNotes }}</p>
-    </div>
+    <appointment-list :appointments="appointments"/>
   </div>
 </template>
 
 <script>
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import AppointmentList from './components/AppointmentList'
 import axios from 'axios'
 
 export default {
@@ -23,7 +17,7 @@ export default {
     };
   },
   components: {
-    FontAwesomeIcon
+    AppointmentList
   },
   mounted() {
     axios
