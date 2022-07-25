9d70d14b56ff8d809ac70b9e26ee6ee593b2609c
serhiichaiuk <chayck7@gmail.com>
2022-07-25 17:48:01 +0300

    added font awesome icons

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faPlus, faMinus, faTrash, faCheck
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlus, faMinus, faTrash, faCheck)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
