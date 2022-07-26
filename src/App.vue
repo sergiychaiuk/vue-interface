9edfaa928f3f66e6bcc827921acd971b74671777
serhiichaiuk <chayck7@gmail.com>
2022-07-26 14:00:34 +0300

    added filtered computed property

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="row justify-content-center">
       <add-appointment @add="addItem"/>
       <search-appointments @searchRecords="searchAppointments"/>
-      <appointment-list :appointments="searchedApts" @remove="removeItem" @edit="editItem"/>
+      <appointment-list :appointments="filteredApts" @remove="removeItem" @edit="editItem"/>
     </div>
   </div>
 </template>
@@ -21,6 +21,8 @@ export default {
     return {
       title: 'Appointment List',
       appointments: [],
+      filterKey: 'petName',
+      filterDir: 'asc',
       searchTerms: '',
       aptIndex: 0
     };
@@ -39,6 +41,15 @@ export default {
             item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
         );
       });
+    },
+    filteredApts: function() {
+      return _.orderBy(
+          this.searchedApts,
+          item => {
+            return item[this.filterKey].toLowerCase();
+          },
+          this.filterDir
+      );
     }
   },
   mounted() {
